e4dd86ad0d563aaf65954f7699cd8b7c646f0781
royweisfeld <roy.weisfeld+git@hotmail.com>
2024-09-22 14:32:28 +0300

    Revert "dolby: Introduce graphical equalizer"
    This reverts commit 55a6d72fe3bc50eed32572640758a2693555c229.

--- dolby/src/co/aospa/dolby/xiaomi/DolbySettingsFragment.kt
+++ dolby/src/co/aospa/dolby/xiaomi/DolbySettingsFragment.kt
@@ -34,7 +34,7 @@ class DolbySettingsFragment : PreferenceFragment(),
         findPreference<ListPreference>(DolbyConstants.PREF_PROFILE)!!
     }
     private val presetPref by lazy {
-        findPreference<Preference>(DolbyConstants.PREF_PRESET)!!
+        findPreference<ListPreference>(DolbyConstants.PREF_PRESET)!!
     }
     private val stereoPref by lazy {
         findPreference<ListPreference>(DolbyConstants.PREF_STEREO)!!
@@ -106,6 +106,7 @@ class DolbySettingsFragment : PreferenceFragment(),
             }
         }
 
+        presetPref.onPreferenceChangeListener = this
         hpVirtPref.onPreferenceChangeListener = this
         spkVirtPref.onPreferenceChangeListener = this
         stereoPref.onPreferenceChangeListener = this
@@ -135,11 +136,6 @@ class DolbySettingsFragment : PreferenceFragment(),
         super.onDestroyView()
     }
 
-    override fun onResume() {
-        super.onResume()
-        updateProfileSpecificPrefs()
-    }
-
     override fun onPreferenceChange(preference: Preference, newValue: Any): Boolean {
         dlog(TAG, "onPreferenceChange: key=${preference.key} value=$newValue")
         when (preference.key) {
@@ -148,6 +144,10 @@ class DolbySettingsFragment : PreferenceFragment(),
                 dolbyController.profile = profile
                 (preferenceManager.preferenceDataStore as DolbyPreferenceStore).profile = profile
                 updateProfileSpecificPrefs()
+            }
+
+            DolbyConstants.PREF_PRESET -> {
+                dolbyController.setPreset(newValue.toString())
             }
 
             DolbyConstants.PREF_SPK_VIRTUALIZER -> {
@@ -214,7 +214,15 @@ class DolbySettingsFragment : PreferenceFragment(),
 
         if (!enable) return
 
-        presetPref.summary = dolbyController.getPresetName()
+        val preset = dolbyController.getPreset(currentProfile)
+        presetPref.apply {
+            if (entryValues.contains(preset)) {
+                summary = "%s"
+                value = preset
+            } else {
+                summary = unknownRes
+            }
+        }
 
         val deValue = dolbyController.getDialogueEnhancerAmount(currentProfile).toString()
         dialoguePref.apply {
